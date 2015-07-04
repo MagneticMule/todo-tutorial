67f4989f51d64929ae8e4e942e32e0ae323206e6
Thomas Sweeney <skywriter@gmail.com>
2015-07-04 12:11:21 +0100

    Just in case

--- todo-tut.js
+++ todo-tut.js
@@ -11,8 +11,8 @@ if (Meteor.isClient) {
 
       var t = event.target.text.value;
       if(t!==""){
-          Tasks.insert({
-            text: t,
+        Tasks.insert({
+          text: t,
             createdAt: new Date() // the current date and time
           });
       }
@@ -34,7 +34,9 @@ if (Meteor.isClient) {
 
   Template.task.events({
     // Modify an entry based on it's ID
-
+    'click .toggle-checked': function() {
+      Tasks.update(this._id,{$set: {checked: !this.checked}});
+    },
 
     // Delete an entry based on it's ID
     'click .delete': function () {
